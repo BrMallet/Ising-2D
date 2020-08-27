e04937eac00b1828a24ee0a39e24767e1b318ed0
BrMallet <brunomallet@id.uff.br>
2020-08-27 07:46:18 -0300

    commit massage

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,17 @@ use rand::Rng;
 use std::borrow::Borrow;
 
 #[derive(Debug, PartialEq)]
+
+// Criaçao do spin
+
 enum SpinValue {
     Positive,
     Negative,
 }
+
+//--------------------
+
+// Flip de Spin
 
 fn negate_spin_value(v: &SpinValue) -> SpinValue {
     if *v == SpinValue::Positive {
@@ -16,6 +23,10 @@ fn negate_spin_value(v: &SpinValue) -> SpinValue {
         SpinValue::Positive
     }
 }
+
+//--------------------------------
+
+// Criação da rede 2d aleatoria
 
 fn random_2d_network(rows: usize, columns: usize) -> Vec<Vec<SpinValue>> {
     let mut rng = rand::thread_rng();
@@ -32,21 +43,24 @@ fn random_2d_network(rows: usize, columns: usize) -> Vec<Vec<SpinValue>> {
     spins
 }
 
+//------------------------------
 
+// Inversão de Spin
 
-fn should_change(delta: f64, h: f64) -> bool {
+fn should_change(delta: f64, beta: f64) -> bool {
     let mut rng = rand::thread_rng();
     let change = rng.gen_range(0 as f64, 1 as f64);
-    if change < (delta * h).exp() { true } else { false }
+    if change < (-delta * beta).exp() { true } else { false }
 }
 
-// Changes ---------------------------------------------------------------------------------
+//--------------------------
+
 
 // Neighbor spin sum
 
 fn neighbor_interection(s: &SpinValue, s_iplus1_j: &SpinValue, s_iminus1_j: &SpinValue, s_i_jplus1: &SpinValue, s_ijminus1: &SpinValue) -> f64 {
 
-    let mut s_n: f64 = 0.0; let mut s_0: f64 = 0.0; let mut s_1: f64 = 0.0; let mut s_2: f64 = 0.0; let mut s_3: f64 = 0.0; let mut s_4: f64 = 0.0;
+    let mut s_0: f64 = 0.0; let mut s_1: f64 = 0.0; let mut s_2: f64 = 0.0; let mut s_3: f64 = 0.0; let mut s_4: f64 = 0.0;
 
     if *s == SpinValue::Positive { s_0 = 1.0
     } else { s_0 = -1.0 }
@@ -54,22 +68,30 @@ fn neighbor_interection(s: &SpinValue, s_iplus1_j: &SpinValue, s_iminus1_j: &Spi
     if *s_iplus1_j == SpinValue::Positive { s_1 = 1.0
     } else { s_1 = -1.0 }
 
-    if *s_iminus1_j == SpinValue::Positive { s_2 = 1.0
-    } else { s_2 = -1.0 }
+    if *s_iminus1_j == SpinValue::Positive { 
+        s_2 = 1.0
+    } else { 
+        s_2 = -1.0 }
 
-    if *s_i_jplus1 == SpinValue::Positive { s_3 = 1.0
-    } else { s_3 = -1.0 }
+    if *s_i_jplus1 == SpinValue::Positive { 
+        s_3 = 1.0
+    } else { 
+        s_3 = -1.0 }
 
-    if *s_ijminus1 == SpinValue::Positive { s_4 = 1.0
-    } else { s_4 = -1.0 }
+    if *s_ijminus1 == SpinValue::Positive { 
+        s_4 = 1.0
+    } else { 
+        s_4 = -1.0 }
 
-    s_n = s_0 + s_1 + s_2 + s_3 + s_4;
+    let s_n: f64 =  s_0 * (s_1 + s_2 + s_3 + s_4);
 
     s_n
 
 }
 
 // --------------------------------------
+
+// Termos vizinhos
 
 fn next_index(current_index: usize, max_index: usize) -> usize {
     if current_index == max_index - 1 {
@@ -87,127 +109,171 @@ fn prev_index(current_index: usize, max_index: usize) -> usize {
     }
 }
 
+//------------------
+
+//Spin Total novo 
+
+fn s_total_novo (s: &SpinValue,beta: f64,h: f64) -> f64 {
+
+
+    if *s == SpinValue::Positive {
+        let h_1: f64 = -2.0*beta*h;  
+        h_1    
+    } else {
+        let h_1: f64 = 2.0*beta*h;        
+        h_1
+    }
+    
+}
+
+//----------------------------
+
+//Interaçao visinha nova
+
+fn visinho_novo (s: &SpinValue,s_visinho: &SpinValue,beta: f64,j: f64) -> f64 {
+    if *s == *s_visinho {
+        let h_1: f64 = -2.0*beta*j;  
+        h_1
+    } else {
+        let h_1: f64 = 2.0*beta*j;  
+        h_1 
+    }
+}
+
+////-------------------------
 fn main() {
     /* Passo 1: Criação de uma janela e um objeto renderizador de linhas */
     let (mut renderer, mut window) = renderer::setup_renderer();
 
-    /* Passo 2: Loop infinito renderizando as linhas */
+   /* Passo 2: Loop infinito renderizando as linhas */
     renderer::render_in_a_loop(&mut window, &mut renderer);
-
-
-    // Set realistic parameters here
-    // let net_size_r = 3; let net_size_c = 3;
-    // let mut random_network = random_2d_network(net_size_r, net_size_c);
-    // println!("Random network before: {:?}", random_network);
-    // let j: f64 = 1.0; let beta: f64 = 2.0; let h: f64 = 3.0;
-    // // -----------------------------
-    //
-    // let mut thermal_balance = false;
-    // while !thermal_balance {
-    //     let mut rng = rand::thread_rng();
-    //
-    //
-    //     let i_ind: usize = rng.gen_range(0 as usize, net_size_r);
-    //     let j_ind: usize = rng.gen_range(0 as usize, net_size_c);
-    //
-    //
-    //     println!("Indice I: {:?}", i_ind);
-    //     println!("Indice J: {:?}", j_ind);
-    //
-    //     let mut e_0: f64 = 0.0; let mut e_1: f64 = 0.0; let mut delta: f64 = 0.0;
-    //
-    //     for m in 0..2 {
-    //         let s_ij = random_network[i_ind][j_ind].borrow();
-    //
-    //
-    //         //Changes -----------------------------------------------------------
-    //
-    //
-    //         // Neighbors parameter ---------------------
-    //
-    //         let i_ind_plus1 = next_index(i_ind, net_size_r);
-    //         let i_ind_minus1 = prev_index(i_ind, net_size_r);
-    //         let j_ind_plus1 = next_index(j_ind, net_size_c);
-    //         let j_ind_minus1 = prev_index(j_ind, net_size_c);
-    //
-    //         println!("Indice I+1: {:?}", i_ind_plus1);
-    //         println!("Indice J+1: {:?}", j_ind_plus1);
-    //         println!("Indice I-1: {:?}", i_ind_minus1);
-    //         println!("Indice J-1: {:?}", j_ind_minus1);
-    //
-    //
-    //         let s_iplus1_j = random_network[i_ind_plus1][j_ind].borrow();
-    //         let s_iminus1_j = random_network[i_ind_minus1][j_ind].borrow();
-    //         let s_i_jplus1 = random_network[i_ind][j_ind_plus1].borrow();
-    //         let s_ijminus1 = random_network[i_ind][j_ind_minus1].borrow();
-    //
-    //         println!("Spin I+1 J: {:?}", s_iplus1_j);
-    //         println!("Spin I J+1: {:?}", s_iminus1_j);
-    //         println!("Spin I-1 J: {:?}", s_i_jplus1);
-    //         println!("Spin I J-1: {:?}", s_ijminus1);
-    //         println!("Spin I J: {:?}", s_ij);
-    //
-    //
-    //         // Total spin sum ---------------
-    //
-    //         let mut s_total: f64 = 0.0;
-    //
-    //         for l in 0..net_size_r {
-    //             for k in 0..net_size_c {
-    //                 let i_ind = l;
-    //                 let j_ind = k;
-    //                 let s_ij = random_network[i_ind][j_ind].borrow();
-    //                 if *s_ij == SpinValue::Positive {
-    //                     s_total = s_total + 1.0
-    //                 } else {
-    //                     s_total = s_total - 1.0
-    //                 }
-    //             }
-    //         }
-    //
-    //         println!("Indice I: {:?}", i_ind);
-    //         println!("Indice J: {:?}", j_ind);
-    //
-    //         // Total Energy Sum -----------------
-    //
-    //         let alpha = neighbor_interection(s_ij, s_iplus1_j, s_iminus1_j, s_i_jplus1, s_ijminus1);
-    //
-    //         let gama = s_total;
-    //
-    //         e_0 = beta * (-j * alpha - h * gama);
-    //
-    //         println!("E_0:{:?}", e_0);
-    //         println!("Spin total:{:?}", gama);
-    //         println!("Primeiros vizinhos:{:?}", alpha);
-    //
-    //         delta = e_1.exp() - e_0.exp();
-    //
-    //         e_1 = e_0;
-    //
-    //         random_network[i_ind][j_ind] = negate_spin_value(random_network[i_ind][j_ind].borrow());
-    //
-    //     }
-    //
-    //     println! ("Delta E:{:?}", delta);
-    //
-    //
-    //     // We should do the calculus again with negate s_ij to get delta
-    //
-    //
-    //
-    //     // ----------------------------------------
-    //
-    //     if delta <= 0.0 {
-    //         random_network[i_ind][j_ind] = negate_spin_value(random_network[i_ind][j_ind].borrow())
-    //     } else {
-    //         if should_change(delta, h) {
-    //             random_network[i_ind][j_ind] = negate_spin_value(random_network[i_ind][j_ind].borrow())
-    //         }
-    //     }
-    //
-    //     // Then I guess we should decide what to do with thermal_balance
-    //     // But for now this is enough
-    //     thermal_balance = true;
-    // }
-    // println!("Random network after: {:?}", random_network);
+//
+//
+//    // Montagem da rede
+//
+//    let net_size_r = 30; let net_size_c = 30;
+//    let mut random_network = random_2d_network(net_size_r, net_size_c);
+//
+//    //--------------------------------
+//
+//    //Parametros fisicos
+//    
+//    let j: f64 = 1.0; let h: f64 = 0.1; let t: f64 = 2.7; 
+//    let beta: f64 = 1.0/ t ;
+//
+//    // -----------------------------
+//    
+//    //Loop -----------------------
+//
+//    for _z in 0 .. 100000000   {   
+//     
+//
+//        //Spin aleatorio
+//
+//        let mut rng = rand::thread_rng();
+//
+//
+//        let i_ind: usize = rng.gen_range(0 as usize, net_size_r);
+//        let j_ind: usize = rng.gen_range(0 as usize, net_size_c); 
+//        let s_ij = random_network[i_ind][j_ind].borrow();
+//
+//        let i_ind_plus1 = next_index(i_ind, net_size_r);
+//        let i_ind_minus1 = prev_index(i_ind, net_size_r);
+//        let j_ind_plus1 = next_index(j_ind, net_size_c);
+//        let j_ind_minus1 = prev_index(j_ind, net_size_c);
+//
+//        let s_iplus1_j = random_network[i_ind_plus1][j_ind].borrow();
+//        let s_iminus1_j = random_network[i_ind_minus1][j_ind].borrow();
+//        let s_i_jplus1 = random_network[i_ind][j_ind_plus1].borrow();
+//        let s_ijminus1 = random_network[i_ind][j_ind_minus1].borrow();
+//        
+//        //-------------------------
+//
+//        //Calculo de Delta
+//
+//        let h_1 = s_total_novo(s_ij, beta, h);
+//        let h_2 = visinho_novo(s_ij, s_iplus1_j, beta, j);
+//        let h_3 = visinho_novo(s_ij, s_iminus1_j, beta, j);
+//        let h_4 = visinho_novo(s_ij, s_i_jplus1, beta, j);
+//        let h_5 = visinho_novo(s_ij, s_ijminus1, beta, j);
+//
+//        let delta: f64 = h_1 + h_2 + h_3 + h_4 + h_5 ;
+//
+//        //--------------------------
+//
+//        //Inversão do Spin
+//
+//        if delta <= 0.0 {
+//            random_network[i_ind][j_ind] = negate_spin_value(random_network[i_ind][j_ind].borrow())
+//        } else {
+//            if should_change(delta, beta) {
+//                random_network[i_ind][j_ind] = negate_spin_value(random_network[i_ind][j_ind].borrow())
+//            }
+//        }
+//        //------------------------
+//    } 
+//
+//    //------------------------
+//
+//    //Valores Medios das Grandezas Termodinamicas
+//
+//    let mut s_total: f64 = 0.0; let mut alpha: f64 = 0.0; let mut h_sqr: f64 = 0.0; let mut s: f64 = 0.0;
+//
+//        for l in 0..net_size_r {
+//            for k in 0..net_size_c {
+//                let i_ind = l;
+//                let j_ind = k;
+//
+//                let s_ij = random_network[i_ind][j_ind].borrow();
+//
+//                if *s_ij == SpinValue::Positive {
+//                    s_total = s_total + 1.0 ;
+//                    s = 1.0
+//                } else {
+//                    s_total = s_total - 1.0 ;
+//                    s = -1.0
+//                }
+//
+//                let i_ind_plus1 = next_index(i_ind, net_size_r);
+//                let i_ind_minus1 = prev_index(i_ind, net_size_r);
+//                let j_ind_plus1 = next_index(j_ind, net_size_c);
+//                let j_ind_minus1 = prev_index(j_ind, net_size_c);
+//
+//                let s_iplus1_j = random_network[i_ind_plus1][j_ind].borrow();
+//                let s_iminus1_j = random_network[i_ind_minus1][j_ind].borrow();
+//                let s_i_jplus1 = random_network[i_ind][j_ind_plus1].borrow();
+//                let s_ijminus1 = random_network[i_ind][j_ind_minus1].borrow();                
+//                                    
+//                let vizinho = neighbor_interection(s_ij, s_iplus1_j, s_iminus1_j, s_i_jplus1, s_ijminus1);
+//
+//                alpha = (alpha + vizinho)/2.0 ;
+//
+//                let sqr : f64 = (-j * vizinho - h * s).powf(2.0);
+//
+//                h_sqr  = sqr + h_sqr
+//
+//            }
+//        }
+//    
+//    let n_c: f64 = net_size_c as f64;
+//    let n_r: f64 = net_size_r as f64;
+//
+//        
+//    let m: f64 = s_total/(n_c*n_r);
+//
+//    println!("Magnetização média: {:?}", m);
+//
+//    let h_medio: f64 = (-j * alpha - h * s_total)/(n_c*n_r);
+//
+//    println!("Energia média: {:?}", h_medio);
+//
+//    let c_v: f64 = (h_sqr/(n_c*n_r) - (h_medio).powf(2.0))/(t).powf(2.0);
+//
+//    println!("Calor especifico: {:?}", c_v);
+//
+//    let x_m: f64 = (n_c*n_r - (m).powf(2.0))/(t).powf(2.0) ;
+//
+//    println!("Suscetibilidade magnetica: {:?}", x_m);   
+//
+//    //-------------------------------------------------
 }
